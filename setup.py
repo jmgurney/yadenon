1e2db279e608a2550f6a7d683be0aca2e6fd3df6
John-Mark Gurney <jmg@funkthat.com>
2017-07-01 15:34:42 -0700

    add module setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(name='yadenon',
+      version='1.0.0',
+      description='Yet Anoter Python Denon AVR module',
+      author='John-Mark Gurney',
+      author_email='jmg@funkthat.com',
+      url='https://github.com/jmgurney/yadenon',
+      py_modules=['yadenon'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          ],
+     )
